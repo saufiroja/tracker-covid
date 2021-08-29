f83645e26316dc2a55f51c073966cfcb8221ca66
saufiroja <saufiroja08@gmail.com>
2021-08-29 12:38:49 +0800

    mengimport data api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
+import styles from "./App.module.css";
 
 export default class App extends Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fatchedData = await fetchData();
+    this.setState({ data: fatchedData });
+  }
+
   render() {
+    const { data } = this.state;
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
